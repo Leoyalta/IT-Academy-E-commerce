15baa24057ad66a6eefea3b9b45f0d2b158ae66d
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-04-03 10:52:50 +0200

    Add-README

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,6 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-  {
-    id: 1,
-    name: "cooking oil",
-    price: 10.5,
-    type: "grocery",
-    offer: {
-      number: 3,
-      percent: 20,
-    },
-  },
-  {
-    id: 2,
-    name: "Pasta",
-    price: 6.25,
-    type: "grocery",
-  },
-  {
-    id: 3,
-    name: "Instant cupcake mixture",
-    price: 5,
-    type: "grocery",
-    offer: {
-      number: 10,
-      percent: 30,
-    },
-  },
-  {
-    id: 4,
-    name: "All-in-one",
-    price: 260,
-    type: "beauty",
-  },
-  {
-    id: 5,
-    name: "Zero Make-up Kit",
-    price: 20.5,
-    type: "beauty",
-  },
-  {
-    id: 6,
-    name: "Lip Tints",
-    price: 12.75,
-    type: "beauty",
-  },
-  {
-    id: 7,
-    name: "Lawn Dress",
-    price: 15,
-    type: "clothes",
-  },
-  {
-    id: 8,
-    name: "Lawn-Chiffon Combo",
-    price: 19.99,
-    type: "clothes",
-  },
-  {
-    id: 9,
-    name: "Toddler Frock",
-    price: 9.99,
-    type: "clothes",
-  },
-];
+import { products } from "../products.js";
+console.log(products);
 
 // => Reminder, it's extremely important that you debug your code.
 // ** It will save you a lot of time and frustration!
@@ -192,12 +130,12 @@ cartList.addEventListener("click", (event) => {
   }
 });
 
-function addToCart(id) {
-  const cartItem = cart.find((item) => item.id === id);
-  cartItem && (cartItem.quantity += 1);
-  cartBtnValue.innerHTML = parseInt(cartBtnValue.innerHTML) + 1;
-  printCart();
-}
+// function addToCart(id) {
+//   const cartItem = cart.find((item) => item.id === id);
+//   cartItem && (cartItem.quantity += 1);
+//   cartBtnValue.innerHTML = parseInt(cartBtnValue.innerHTML) + 1;
+//   printCart();
+// }
 
 function removeFromCart(id) {
   const cartItem = cart.find((item) => item.id === id);
